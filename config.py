31c9462c1e1b9cb6280b1e25e92763714139f175
SPIDEY OFFICIAL <gaminghatyar777@gmail.com>
2025-03-15 14:12:12 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -1,14 +1,28 @@
+import datetime
 from os import environ 
 
-class Config:
-    API_ID = environ.get("API_ID", "577678")
-    API_HASH = environ.get("API_HASH", "d2c6e01uuiuiouioiuiou0fc6d7a1be")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "70955...") 
-    BOT_SESSION = environ.get("BOT_SESSION", "bot") 
-    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://chhjgjkkjhkjhkjh@cluster0.xowzpr4.mongodb.net/")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "forward-bot")
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6964148334').split()]
+#Dont Remove My Credit @Silicon_Bot_Update 
+#This Repo Is By @Silicon_Official 
+# For Any Kind Of Error Ask Us In Support Group @Silicon_Botz 
 
+class Config:
+    API_ID = environ.get("API_ID", "28519661")
+    API_HASH = environ.get("API_HASH", "d47c74c8a596fd3048955b322304109d")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "") 
+    BOT_SESSION = environ.get("BOT_SESSION", "Auto_Forward") 
+    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://voromil970:q9aLNL7nsD8EDzwL@cluster0.rlvax.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "SPIDEY777")
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '5518489725').split()]
+    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002423451263'))
+    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "-1001959922658") 
+    FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")
+    PORT = environ.get('PORT', '8080')
+    
+#Dont Remove My Credit @Silicon_Bot_Update 
+#This Repo Is By @Silicon_Official 
+# For Any Kind Of Error Ask Us In Support Group @Silicon_Botz 
+
+   
 class temp(object): 
     lock = {}
     CANCEL = {}
@@ -16,3 +30,6 @@ class temp(object):
     BANNED_USERS = []
     IS_FRWD_CHAT = []
     
+#Dont Remove My Credit @Silicon_Bot_Update 
+#This Repo Is By @Silicon_Official 
+# For Any Kind Of Error Ask Us In Support Group @Silicon_Botz 
